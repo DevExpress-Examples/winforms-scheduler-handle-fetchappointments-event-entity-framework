6b20553a0d23bad16c0a765e0756982fbc102262
DevExpressExampleBot <fakeEmail>
2023-09-20 17:57:16 +0400

    Source auto update [skip ci]

--- VB/Form1.Designer.vb
+++ VB/Form1.Designer.vb
@@ -1,82 +1,79 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace SchedulerEntityFramework
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.components = New System.ComponentModel.Container()
-			Dim timeRuler1 As New DevExpress.XtraScheduler.TimeRuler()
-			Dim timeRuler2 As New DevExpress.XtraScheduler.TimeRuler()
-			Me.schedulerControl1 = New DevExpress.XtraScheduler.SchedulerControl()
-			Me.schedulerStorage1 = New DevExpress.XtraScheduler.SchedulerStorage(Me.components)
-			CType(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' schedulerControl1
-			' 
-			Me.schedulerControl1.ActiveViewType = DevExpress.XtraScheduler.SchedulerViewType.Timeline
-			Me.schedulerControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.schedulerControl1.GroupType = DevExpress.XtraScheduler.SchedulerGroupType.Resource
-			Me.schedulerControl1.Location = New System.Drawing.Point(0, 0)
-			Me.schedulerControl1.Name = "schedulerControl1"
-			Me.schedulerControl1.Size = New System.Drawing.Size(687, 489)
-			Me.schedulerControl1.Start = New System.DateTime(2013, 5, 16, 0, 0, 0, 0)
-			Me.schedulerControl1.Storage = Me.schedulerStorage1
-			Me.schedulerControl1.TabIndex = 0
-			Me.schedulerControl1.Text = "schedulerControl1"
-			Me.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler1)
-			Me.schedulerControl1.Views.TimelineView.ResourcesPerPage = 3
-			Me.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler2)
-			' 
-			' schedulerStorage1
-			' 
-			Me.schedulerStorage1.Appointments.ResourceSharing = True
-'			Me.schedulerStorage1.AppointmentsInserted += New DevExpress.XtraScheduler.PersistentObjectsEventHandler(Me.schedulerStorage1_AppointmentsInserted);
-'			Me.schedulerStorage1.AppointmentsChanged += New DevExpress.XtraScheduler.PersistentObjectsEventHandler(Me.schedulerStorage1_AppointmentsChanged);
-'			Me.schedulerStorage1.AppointmentsDeleted += New DevExpress.XtraScheduler.PersistentObjectsEventHandler(Me.schedulerStorage1_AppointmentsDeleted);
-'			Me.schedulerStorage1.FetchAppointments += New DevExpress.XtraScheduler.FetchAppointmentsEventHandler(Me.Storage_FetchAppointments);
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(8F, 16F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(687, 489)
-			Me.Controls.Add(Me.schedulerControl1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-			CType(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+'#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Dim timeRuler1 As DevExpress.XtraScheduler.TimeRuler = New DevExpress.XtraScheduler.TimeRuler()
+            Dim timeRuler2 As DevExpress.XtraScheduler.TimeRuler = New DevExpress.XtraScheduler.TimeRuler()
+            Me.schedulerControl1 = New DevExpress.XtraScheduler.SchedulerControl()
+            Me.schedulerStorage1 = New DevExpress.XtraScheduler.SchedulerStorage(Me.components)
+            CType((Me.schedulerControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.schedulerStorage1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' schedulerControl1
+            ' 
+            Me.schedulerControl1.ActiveViewType = DevExpress.XtraScheduler.SchedulerViewType.Timeline
+            Me.schedulerControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.schedulerControl1.GroupType = DevExpress.XtraScheduler.SchedulerGroupType.Resource
+            Me.schedulerControl1.Location = New System.Drawing.Point(0, 0)
+            Me.schedulerControl1.Name = "schedulerControl1"
+            Me.schedulerControl1.Size = New System.Drawing.Size(687, 489)
+            Me.schedulerControl1.Start = New System.DateTime(2013, 5, 16, 0, 0, 0, 0)
+            Me.schedulerControl1.Storage = Me.schedulerStorage1
+            Me.schedulerControl1.TabIndex = 0
+            Me.schedulerControl1.Text = "schedulerControl1"
+            Me.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler1)
+            Me.schedulerControl1.Views.TimelineView.ResourcesPerPage = 3
+            Me.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler2)
+            ' 
+            ' schedulerStorage1
+            ' 
+            Me.schedulerStorage1.Appointments.ResourceSharing = True
+            AddHandler Me.schedulerStorage1.AppointmentsInserted, New DevExpress.XtraScheduler.PersistentObjectsEventHandler(AddressOf Me.schedulerStorage1_AppointmentsInserted)
+            AddHandler Me.schedulerStorage1.AppointmentsChanged, New DevExpress.XtraScheduler.PersistentObjectsEventHandler(AddressOf Me.schedulerStorage1_AppointmentsChanged)
+            AddHandler Me.schedulerStorage1.AppointmentsDeleted, New DevExpress.XtraScheduler.PersistentObjectsEventHandler(AddressOf Me.schedulerStorage1_AppointmentsDeleted)
+            AddHandler Me.schedulerStorage1.FetchAppointments, New DevExpress.XtraScheduler.FetchAppointmentsEventHandler(AddressOf Me.Storage_FetchAppointments)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(8F, 16F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(687, 489)
+            Me.Controls.Add(Me.schedulerControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType((Me.schedulerControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.schedulerStorage1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private schedulerControl1 As DevExpress.XtraScheduler.SchedulerControl
-		Private WithEvents schedulerStorage1 As DevExpress.XtraScheduler.SchedulerStorage
+'#End Region
+        Private schedulerControl1 As DevExpress.XtraScheduler.SchedulerControl
 
-	End Class
+        Private schedulerStorage1 As DevExpress.XtraScheduler.SchedulerStorage
+    End Class
 End Namespace
-
--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,79 +1,67 @@
-Imports Microsoft.VisualBasic
 Imports System
 Imports System.Linq
 Imports System.Windows.Forms
 Imports DevExpress.XtraScheduler
 
 Namespace SchedulerEntityFramework
-	Partial Public Class Form1
-		Inherits Form
-		Private carsXtraSchedulingEntities As New CarsXtraSchedulingEntities()
-		Private lastFetchedInterval As New TimeInterval()
 
-		Public Sub New()
-			InitializeComponent()
+    Public Partial Class Form1
+        Inherits Form
 
-			SetupMappings()
+        Private carsXtraSchedulingEntities As CarsXtraSchedulingEntities = New CarsXtraSchedulingEntities()
 
-			schedulerControl1.Storage.Appointments.DataSource = New BindingSource()
-			schedulerControl1.Storage.Resources.DataSource = carsXtraSchedulingEntities.Cars
+        Private lastFetchedInterval As TimeInterval = New TimeInterval()
 
-			schedulerControl1.Start = New System.DateTime(2008, 7, 1)
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+            SetupMappings()
+            schedulerControl1.Storage.Appointments.DataSource = New BindingSource()
+            schedulerControl1.Storage.Resources.DataSource = carsXtraSchedulingEntities.Cars
+            schedulerControl1.Start = New DateTime(2008, 7, 1)
+        End Sub
 
-		Private Sub schedulerStorage1_AppointmentsInserted(ByVal sender As Object, ByVal e As DevExpress.XtraScheduler.PersistentObjectsEventArgs) Handles schedulerStorage1.AppointmentsInserted
-			carsXtraSchedulingEntities.SaveChanges()
+        Private Sub schedulerStorage1_AppointmentsInserted(ByVal sender As Object, ByVal e As PersistentObjectsEventArgs)
+            carsXtraSchedulingEntities.SaveChanges()
+            For i As Integer = 0 To e.Objects.Count - 1
+                Dim apt As Appointment = CType(e.Objects(i), Appointment)
+                Dim source As CarScheduling = CType(apt.GetSourceObject(schedulerStorage1), CarScheduling)
+                schedulerStorage1.SetAppointmentId(apt, source.ID)
+            Next
+        End Sub
 
-			For i As Integer = 0 To e.Objects.Count - 1
-				Dim apt As Appointment = CType(e.Objects(i), Appointment)
-				Dim source As CarScheduling = CType(apt.GetSourceObject(schedulerStorage1), CarScheduling)
+        Private Sub schedulerStorage1_AppointmentsChanged(ByVal sender As Object, ByVal e As PersistentObjectsEventArgs)
+            carsXtraSchedulingEntities.SaveChanges()
+        End Sub
 
-				schedulerStorage1.SetAppointmentId(apt, source.ID)
-			Next i
-		End Sub
+        Private Sub schedulerStorage1_AppointmentsDeleted(ByVal sender As Object, ByVal e As PersistentObjectsEventArgs)
+            carsXtraSchedulingEntities.SaveChanges()
+        End Sub
 
-		Private Sub schedulerStorage1_AppointmentsChanged(ByVal sender As Object, ByVal e As DevExpress.XtraScheduler.PersistentObjectsEventArgs) Handles schedulerStorage1.AppointmentsChanged
-			carsXtraSchedulingEntities.SaveChanges()
-		End Sub
+        Private Sub Storage_FetchAppointments(ByVal sender As Object, ByVal e As FetchAppointmentsEventArgs)
+            Dim bindingSource As BindingSource = TryCast(schedulerControl1.Storage.Appointments.DataSource, BindingSource)
+            If bindingSource Is Nothing OrElse lastFetchedInterval.Contains(e.Interval) Then Return
+            Dim margin As TimeSpan = TimeSpan.FromDays(0) ' TimeSpan.FromDays(1) 
+            lastFetchedInterval = New TimeInterval(e.Interval.Start - margin, e.Interval.End + margin)
+            Dim entities = From entity In carsXtraSchedulingEntities.CarSchedulings Where entity.EventType = 1 OrElse entity.StartTime < lastFetchedInterval.End AndAlso entity.EndTime > lastFetchedInterval.Start Select entity
+            bindingSource.DataSource = entities
+        End Sub
 
-		Private Sub schedulerStorage1_AppointmentsDeleted(ByVal sender As Object, ByVal e As DevExpress.XtraScheduler.PersistentObjectsEventArgs) Handles schedulerStorage1.AppointmentsDeleted
-			carsXtraSchedulingEntities.SaveChanges()
-		End Sub
-
-		Private Sub Storage_FetchAppointments(ByVal sender As Object, ByVal e As FetchAppointmentsEventArgs) Handles schedulerStorage1.FetchAppointments
-			Dim bindingSource As BindingSource = TryCast(schedulerControl1.Storage.Appointments.DataSource, BindingSource)
-
-			If bindingSource Is Nothing OrElse lastFetchedInterval.Contains(e.Interval) Then
-				Return
-			End If
-
-			Dim margin As TimeSpan = TimeSpan.FromDays(0) ' TimeSpan.FromDays(1)
-			lastFetchedInterval = New TimeInterval(e.Interval.Start - margin, e.Interval.End + margin)
-
-			Dim entities = _
-				From entity In carsXtraSchedulingEntities.CarSchedulings _
-				Where entity.EventType = 1 OrElse (entity.StartTime < lastFetchedInterval.End AndAlso entity.EndTime > lastFetchedInterval.Start) _
-				Select entity
-
-			bindingSource.DataSource = entities
-		End Sub
-
-		Private Sub SetupMappings()
-			Me.schedulerStorage1.Appointments.Mappings.AppointmentId = "ID"
-			Me.schedulerStorage1.Appointments.Mappings.Subject = "Subject"
-			Me.schedulerStorage1.Appointments.Mappings.Description = "Description"
-			Me.schedulerStorage1.Appointments.Mappings.Location = "Location"
-			Me.schedulerStorage1.Appointments.Mappings.Start = "StartTime"
-			Me.schedulerStorage1.Appointments.Mappings.End = "EndTime"
-			Me.schedulerStorage1.Appointments.Mappings.AllDay = "AllDay"
-			Me.schedulerStorage1.Appointments.Mappings.Label = "Label"
-			Me.schedulerStorage1.Appointments.Mappings.Status = "Status"
-			Me.schedulerStorage1.Appointments.Mappings.Type = "EventType"
-			Me.schedulerStorage1.Appointments.Mappings.RecurrenceInfo = "RecurrenceInfo"
-			Me.schedulerStorage1.Appointments.Mappings.ReminderInfo = "ReminderInfo"
-			Me.schedulerStorage1.Appointments.Mappings.ResourceId = "CarId"
-			Me.schedulerStorage1.Resources.Mappings.Id = "ID"
-			Me.schedulerStorage1.Resources.Mappings.Caption = "Model"
-		End Sub
-	End Class
+        Private Sub SetupMappings()
+            schedulerStorage1.Appointments.Mappings.AppointmentId = "ID"
+            schedulerStorage1.Appointments.Mappings.Subject = "Subject"
+            schedulerStorage1.Appointments.Mappings.Description = "Description"
+            schedulerStorage1.Appointments.Mappings.Location = "Location"
+            schedulerStorage1.Appointments.Mappings.Start = "StartTime"
+            schedulerStorage1.Appointments.Mappings.End = "EndTime"
+            schedulerStorage1.Appointments.Mappings.AllDay = "AllDay"
+            schedulerStorage1.Appointments.Mappings.Label = "Label"
+            schedulerStorage1.Appointments.Mappings.Status = "Status"
+            schedulerStorage1.Appointments.Mappings.Type = "EventType"
+            schedulerStorage1.Appointments.Mappings.RecurrenceInfo = "RecurrenceInfo"
+            schedulerStorage1.Appointments.Mappings.ReminderInfo = "ReminderInfo"
+            schedulerStorage1.Appointments.Mappings.ResourceId = "CarId"
+            schedulerStorage1.Resources.Mappings.Id = "ID"
+            schedulerStorage1.Resources.Mappings.Caption = "Model"
+        End Sub
+    End Class
 End Namespace
